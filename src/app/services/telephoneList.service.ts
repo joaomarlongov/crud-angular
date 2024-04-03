be184d1f6377a74b058e262fda3fbb5715209279
Marlon <marlotejmct@gmail.com>
2024-04-03 18:21:34 -0300

    Authorization

--- src/app/services/telephoneList.service.ts
+++ src/app/services/telephoneList.service.ts
@@ -1,17 +1,36 @@
+// import { Injectable } from "@angular/core";
+// import { HttpClient } from "@angular/common/http";
+// import { Observable } from "rxjs";
+// import { TelephoneList } from "../models/TelephoneList";
+
+// @Injectable()
+//     export class TelephoneListService {
+//         telApiUrl = `https://api.baserow.io/api/database/rows/table/276982/`
+//         constructor(private http: HttpClient) {}
+
+//         getTel(): Observable<TelephoneList[]>{
+//             return this.http.get<TelephoneList[]>(this.telApiUrl)
+//         }
+//     }
+
+
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { TelephoneList } from "../models/TelephoneList";
-import { HttpHeaderResponse, HttpHeaders } from "@angular/common/http";
 
 @Injectable()
-    export class TelephoneListService {
-        telApiUrl = `https://api.baserow.io/api/database/rows/table/276982/`
-        constructor(private http: HttpClient) {}
+export class TelephoneListService {
+    telApiUrl = `https://api.baserow.io/api/database/rows/table/276982/`;
+    token = 'kXZAlDdwLEEPS0QxCqPBHjGOYSgxDvEc';
 
-        getTel(): Observable<TelephoneList[]>{
-            return this.http.get<TelephoneList[]>(this.telApiUrl)
-        }
+    constructor(private http: HttpClient) {}
+
+    getTel(): Observable<TelephoneList[]> {
+        const headers = new HttpHeaders().set('Authorization', `Token ${this.token}`);
+
+        return this.http.get<TelephoneList[]>(this.telApiUrl, { headers });
     }
+}
 
 
